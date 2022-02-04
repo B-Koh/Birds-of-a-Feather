a7c8b983fa8890dddaf64062195f523fc22fb12b
ljars <ljarstad@ucsd.edu>
2022-02-03 16:26:24 -0800

    Empty project.

---
+++ app/src/main/java/com/example/birds_of_a_feather_team_20/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.birds_of_a_feather_team_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Find Friends");
+    }
+}
