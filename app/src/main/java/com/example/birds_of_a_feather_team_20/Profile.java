d60d9a31c37ed4482d74f564641e1251ed5f793f
unknown <adu@ucsd.edu>
2022-02-04 18:45:46 -0800

    Skeletons for image methods in profile

--- app/src/main/java/com/example/birds_of_a_feather_team_20/Profile.java
+++ app/src/main/java/com/example/birds_of_a_feather_team_20/Profile.java
@@ -1,5 +1,7 @@
 package com.example.birds_of_a_feather_team_20;
 
+import android.graphics.Bitmap;
+
 /**
  * This class is for representing student profiles. It stores the student's name and the URL to the
  * student's profile photo. This will eventually contain a List of the student's courses too.
@@ -7,8 +9,9 @@ package com.example.birds_of_a_feather_team_20;
 public class Profile {
     private String name;
     private String photoURL;
-    // private Image photo; // TODO
+    private Bitmap thumbnail; //Compressed image from URL
     // private List<Course> courses; // TODO
+
 
     public Profile(String name, String photoURL) {
         this.name = name;
@@ -26,5 +29,32 @@ public class Profile {
     }
     public void setPhotoURL(String photoURL) {
         this.photoURL = photoURL;
+    }
+
+    /**
+     *
+     *
+     */
+
+
+    /**
+     *
+     * @return Uncompressed bitmap from URL if loading successful, null otherwise.
+     */
+    public Bitmap getPhoto(){
+        //TODO: Download image from URL, uncompressed.
+
+        return null;
+    }
+
+    /**
+     *
+     * @return If image has been previously loaded, null otherwise.
+     */
+    public Bitmap getThumbnail(){
+        //TODO: Download image from URL and compress
+        if(thumbnail != null) return thumbnail;
+
+        return null;
     }
 }
