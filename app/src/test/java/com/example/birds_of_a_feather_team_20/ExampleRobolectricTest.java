dc5b1b7232fcebc52432edc7a6a85580e1ee0af8
ljars <ljarstad@ucsd.edu>
2022-02-07 01:26:01 -0800

    Test assert true

--- app/src/test/java/com/example/birds_of_a_feather_team_20/ExampleRobolectricTest.java
+++ app/src/test/java/com/example/birds_of_a_feather_team_20/ExampleRobolectricTest.java
@@ -8,6 +8,7 @@ import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,11 +16,19 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExampleRobolectricTest {
 //    @Rule
-//    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    }
+
 
     @Test
-    public void test_starts_with_no_result() {
-        assertTrue(true);
+    public void testSucceed() {
+//        ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+//        assertTrue(true);
+        assert true;
         /*// Create a "scenario" to move through the activity lifecycle.
         // https://developer.android.com/guide/components/activities/activity-lifecycle
         ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
@@ -33,4 +42,9 @@ public class ExampleRobolectricTest {
             assertFalse(false);
         });*/
     }
+
+//    @Test
+//    public void testFail() {
+//        assert false;
+//    }
 }
