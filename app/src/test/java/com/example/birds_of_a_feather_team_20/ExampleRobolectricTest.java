da082afbdb51ae2a7ed6f4dda1f85d99362b554d
unknown <adu@ucsd.edu>
2022-02-07 00:58:16 -0800

    Test robolectric integration with github

---
+++ app/src/test/java/com/example/birds_of_a_feather_team_20/ExampleRobolectricTest.java
@@ -0,0 +1,34 @@
+package com.example.birds_of_a_feather_team_20;
+
+import static org.junit.Assert.assertFalse;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleRobolectricTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void test_starts_with_no_result() {
+        // Create a "scenario" to move through the activity lifecycle.
+        // https://developer.android.com/guide/components/activities/activity-lifecycle
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        // Make sure the activity is in the created state (so onCreated is called).
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        // When it's ready, we're ready to test inside this lambda (anonymous inline function).
+        scenario.onActivity(activity -> {
+            // No calculations have been run yet, so there shouldn't be a result!
+            assertFalse(false);
+        });
+    }
+}
